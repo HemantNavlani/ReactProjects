0ea5fe5857a205326f31567c875aca54e2e7adb1
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-03-18 23:17:49 +0530

    currency converter made

--- 04CurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ 04CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -7,7 +7,6 @@ function useCurrencyInfo(currency){
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.3.18/v1/currencies/${currency}.json`)
         .then((res)=>res.json())
         .then((res)=>setData(res[currency]))
-        console.log(data);
     },[currency])
     console.log(data);
     return data
